ba70052c208c81c60b11a8e58d7d993442841180
Albert Pangestu <stamina.left@gmail.com>
2022-07-05 13:43:46 +0700

    Hit API via IBAction

--- MusicLibrary/ViewController.swift
+++ MusicLibrary/ViewController.swift
@@ -20,6 +20,20 @@ class ViewController: UIViewController {
     // MARK: - IBAction
     @IBAction func buttonClicked(_ sender: Any) {
         
+        let apiKey = "9d736e7003289018f7ea496e4e035f3f"
+        let url = URL(string: "https://api.musixmatch.com/ws/1.1/track.search?apikey=\(apiKey)")!
+        let task = URLSession.shared.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }
+            
+            let json = try! JSONSerialization.jsonObject(with: data!, options: []) as! [String: Any]
+            
+            print(json)
+        }
+        task.resume()
     }
     
 }

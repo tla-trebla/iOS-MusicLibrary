1729db7b6038e5c50327e73d9aaf266ad29d2754
Albert Pangestu <stamina.left@gmail.com>
2022-07-05 13:24:57 +0700

    Initial Commit

---
+++ MusicLibrary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicLibrary
+//
+//  Created by Albert Pangestu on 05/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
